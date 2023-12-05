f3d3fb9020ff35643b6ead14408d132c9e2c8151
Rahul Warkade <rahulwarkade954@gmail.com>
2023-12-05 21:44:32 +0530

    boolean

--- variables.ts
+++ variables.ts
@@ -7,10 +7,18 @@ let name: string
     name = "rahul"
     console.log("Hi "+ name+ " what's up!!!");
 
+//number
 let age : number
 
     age = 20
 
     console.log("Age = "+ age)
 
+// boolean
+let male: boolean
+
+    male = true
+
+    console.log(male)
+    
 export {}
